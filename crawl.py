30f17fa16a3cff80c7e734a8af146618491d11f0
yash more1224 <yashsmore7499@gmail.com>
2025-07-11 00:00:38 +0530

    add crawl.py

---
+++ crawl.py
@@ -0,0 +1,33 @@
+from bs4 import BeautifulSoup
+import requests
+
+URL = "https://www.bbc.com/news"
+
+try:
+    page = requests.get(URL)
+    print("got page")
+    # print(page.text)
+
+    soup = BeautifulSoup(page.content, "html.parser")
+    try: 
+        headlines = soup.find_all("h2", class_="sc-9d830f2a-3 jqQlce")
+        sub_headlines = soup.find_all("h2", class_="sc-9d830f2a-3 fWzToZ")
+        most_watched = soup.find_all("h2", class_="sc-9d830f2a-3 UijWH")
+
+        print("\n\nMain Headlines:\n\n")
+        for i, news in enumerate(headlines):
+            print(f"{i+1}. {news.text.strip()}")
+        
+        print("\n\nSmaller Headlines:\n\n")
+        for i, news in enumerate(sub_headlines):
+            print(f"{i+1}. {news.text.strip()}")
+
+        print("\n\nMost watched:\n\n")
+        for i, news in enumerate(most_watched):
+            print(f"{i+1}. {news.text.strip()}")
+
+    except: 
+        print("error fetching headlines")
+except Exception as e:
+    print(f"error occured : {e}")
+
